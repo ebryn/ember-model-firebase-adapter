7838a335de258d4345a5c2242bfb5835a930c914
Erik Bryn <erik.bryn@gmail.com>
2013-07-21 17:28:09 -0500

    Add proper demo

--- demo.js
+++ demo.js
@@ -1,16 +1,32 @@
+var App = Ember.Application.create();
+
 var attr = Ember.attr;
 
 Ember.Model.reopenClass({
   adapter: Ember.FirebaseAdapter.create({rootURL: "https://ember-model-firebase-adapter.firebaseio.com"})
 });
 
-var User = Ember.Model.extend({
+App.Person = Ember.Model.extend({
   id: attr(),
   name: attr()
 });
+App.Person.url = "people";
 
-User.url = "users";
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return App.Person.find();
+  }
+});
 
-// User.create({name: "Erik"}).save();
+App.IndexController = Ember.ArrayController.extend({
+  newName: null,
 
-User.find();
+  save: function() {
+    App.Person.create({name: this.get('newName')}).save();
+    this.set('newName', null);
+  },
+
+  deleteRecord: function(model) {
+    model.deleteRecord();
+  }
+});
